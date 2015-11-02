2078e10660b615e068a023329bf1f0bcbf3ae542
Alexander Borisov <nillerprog@gmail.com>
2015-11-02 20:23:35 +0300

    графика

---
+++ Assets/test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    //GetComponent<Animator>().Play();
+	}
+	
+	
+}
